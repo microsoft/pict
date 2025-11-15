e1f5ee252fe6d6af4d5acaaec187942984214809
Nigel Bess <nigelbess@gmail.com>
2025-11-14 17:14:14 -0800

    unit test improvements

--- PictTests/TestUtil/CombinationGeneratorTest.cs
+++ PictTests/TestUtil/CombinationGeneratorTest.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace PictTests.TestUtil;
 
-namespace PictTests.TestUtil
+[TestClass]
+public class CombinationGeneratorTest
 {
-    internal class CombinationGeneratorTest
+
+    [TestMethod]
+    [DynamicData(nameof(AllPairsCases), DynamicDataSourceType.Method)]
+    public void TestAllPairsPresent(List<int> valueCounts, int order, int expectedNumberOfCombos)
     {
+        var allCombosOfGivenOrder = CombinationGenerator.GenerateAllCombinations(valueCounts, order);
+        Assert.HasCount(expectedNumberOfCombos, allCombosOfGivenOrder);
+
+        var distinctCount = allCombosOfGivenOrder
+            .Select(c => string.Join("|", c.OrderBy(c => c).Select(di => $"{di.Dimension}:{di.Index}")))
+            .Distinct()
+            .Count();
+
+        Assert.AreEqual(expectedNumberOfCombos, distinctCount);
+    }
+
+    public static IEnumerable<object[]> AllPairsCases()
+    {
+        yield return new object[] { new List<int> { 2, 2 }, 2, 4 };
+        yield return new object[] { new List<int> { 2, 3 }, 3, 0 };
+        yield return new object[] { new List<int> { 3, 3, 3 }, 2, 27 };
+        yield return new object[] { new List<int> { 2, 2, 2, 2 }, 2, 24 };
+        yield return new object[] { new List<int> { 2, 3, 4 }, 3, 24 };
     }
 }
